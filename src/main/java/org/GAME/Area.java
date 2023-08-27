eb78edf68593fddbcd9aae4d038fcc845bba6f0e
hudabanimustafa <71215108+hudabanimustafa@users.noreply.github.com>
2023-08-27 19:50:04 +0300

    Add files via upload

--- src/main/java/org/GAME/Area.java
+++ src/main/java/org/GAME/Area.java
@@ -1,2 +1,26 @@
-package org.GAME;public class Area {
+package com;
+
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+public class Area extends Actor {
+	
+	public Area(int x, int y) {
+		super(x, y);
+		
+		initArea();
+	}
+	
+	private void initArea() {
+	
+	ImageIcon icon = new ImageIcon("src/resources/area.png");
+	   /*
+    I don't get image in the brackets, we're not going to write
+    anything, so I will just right here,
+     */
+	Image image = icon.getImage();
+	setImage(image);
+	}
+
 }
