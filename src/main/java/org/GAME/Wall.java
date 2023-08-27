475df42bce463664a0d110e99f0ead0b5a79c216
hudabanimustafa <71215108+hudabanimustafa@users.noreply.github.com>
2023-08-27 19:51:47 +0300

    Add files via upload

--- src/main/java/org/GAME/Wall.java
+++ src/main/java/org/GAME/Wall.java
@@ -1,2 +1,26 @@
-package org.GAME;public class Wall {
+package com;
+
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+public class Wall extends Actor {
+	// add Image
+    // create a new image object
+	private Image image;
+	// METHODS 
+	public Wall(int x, int y) {
+		super(x, y);
+		
+		initWall();
+	}
+
+	private void initWall() {
+		
+		ImageIcon icon = new ImageIcon("src/resources/wall.png");
+		image = icon.getImage();
+		
+		//And this is how we get the image from the folder .
+		setImage(image);
+	}
 }
