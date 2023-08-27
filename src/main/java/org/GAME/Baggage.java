73f474576236b08d7eb2c39cb4ca92eabcd00e61
hudabanimustafa <71215108+hudabanimustafa@users.noreply.github.com>
2023-08-27 19:50:27 +0300

    Add files via upload

--- src/main/java/org/GAME/Baggage.java
+++ src/main/java/org/GAME/Baggage.java
@@ -1,7 +1,32 @@
-package org.GAME;
+package com;
 
+import java.awt.Image;
 
-// It's just basically the back of those who are moving inside the game
+import javax.swing.ImageIcon;
 
-public class bag {
+public class Baggage extends Actor {
+
+    public Baggage(int x, int y) {
+        super(x, y);
+        
+        initBaggage();
+    }
+	
+	private void initBaggage() {
+		
+		ImageIcon icon = new ImageIcon("src/resources/baggage.png");
+		Image image = icon.getImage();
+		setImage(image);
+	}
+	
+    public void move(int x, int y) {
+        
+        int dx = x() + x;
+        int dy = y() + y;
+        
+        setX(dx);
+        setY(dy);
+    }
+	
+
 }
