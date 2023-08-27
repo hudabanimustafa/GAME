6752a33b792b8eebe1d32f035ee3dc78bfe0f823
hudabanimustafa <71215108+hudabanimustafa@users.noreply.github.com>
2023-08-27 19:51:29 +0300

    Add files via upload

--- src/main/java/org/GAME/Player.java
+++ src/main/java/org/GAME/Player.java
@@ -1,2 +1,40 @@
-package org.GAME;public class Player {
+package com;
+
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+public class Player extends Actor {
+	 // The player inherits from Actor
+	
+	public Player(int x, int y) {
+		super(x, y);
+		
+		initPlayer();
+		}
+	
+	private void initPlayer() {
+		// called to new image icon.
+		
+		ImageIcon icon = new ImageIcon("src/resources/actor.png");
+		
+		 /*
+        Objects from the image class, which will be called to icon don't
+        get.
+         */
+		
+		Image image = icon.getImage();
+		setImage(image);
+	}
+	 // move ball
+	public void move(int x, int y) {
+
+        int dx = x() + x;
+        int dy = y() + y;
+        
+        setX(dx);
+        setY(dy);
+		
+	}	
+
 }
