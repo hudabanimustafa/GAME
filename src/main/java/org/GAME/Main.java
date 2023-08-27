d20c5ea35589837a1cd90418627cc500c38f25fc
hudabanimustafa <71215108+hudabanimustafa@users.noreply.github.com>
2023-08-27 19:51:09 +0300

    Add files via upload

--- src/main/java/org/GAME/Main.java
+++ src/main/java/org/GAME/Main.java
@@ -1,7 +1,47 @@
-package org.example;
+package com;
 
-public class Main {
-    public static void main(String[] args) {
-     
-    }
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+/*
+ * My training for the Java course 
+ * An old-fashioned game from your youth ^_^
+ * 27-8-2023
+ * Huda BM 
+ */
+
+public class Main extends JFrame{
+	
+	// create a private variable.
+	private final int OFFSET = 30;
+	//initialize user interface
+	public Main() {
+		initUI();
+	}
+	
+	private void initUI() {
+		
+		Board board = new Board();
+		add(board);
+		
+		setTitle("SBall");
+		
+		setSize(board.getBoardWidth() + OFFSET,
+				board.getBoardHeight() + 2*OFFSET);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	//square built with the initial function.
+		setLocationRelativeTo(null);	
+	}
+	
+	
+	public static void main(String[] args) {
+	//  inside the brackets, let's write our brackets and let's do into this stream.
+		EventQueue.invokeLater(() -> {
+			
+			Main game = new Main();
+			game.setVisible(true);
+		});
+	}
+
 }
